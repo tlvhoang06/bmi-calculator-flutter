7534249d10258dfb9d0a423df2636c6ace447edf
Vu Hoang <truonglevuhoang2707@gmail.com>
2025-08-21 23:22:13 +0700

    Update NDK version, Set up FireBase

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,14 @@ import 'package:demo/views/widget_tree.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
